ab8c720c1d3c060f9bf3e393e437b8c752300ba4
Vladislav Teleshov <Necro01@mail.ru>
2022-06-20 14:36:44 +0300

    Modified

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,35 +1,42 @@
 let nextSlideButton = document.getElementsByClassName('slider__arrow_next')[0]
 let previousSlideButton = document.getElementsByClassName('slider__arrow_prev')[0]
-console.log(nextSlideButton)
 
-let index = 0
+let indexMain = 0;
 
 
 function slider(index) {
 
     let slides = document.getElementsByClassName('slider__item')
 
-    if (index > slides.length - 1) {
-        index = 0
-    }
-    if (index < 0) {
-        index = slides.length - 1
-    }
-
+    
     for (let slide of slides) {
         slide.classList.remove('slider__item_active');
     }
 
-    slides[index].classList.add('slider__item_active')
+    if (index > slides.length - 1) {
+        indexMain = 0;
+        slides[indexMain].classList.add('slider__item_active')
+    }
+
+    if (index < 0) {
+        
+        indexMain = slides.length - 1;
+        slides[indexMain].classList.add('slider__item_active')
+    }
+
+    if ((index <= slides.length - 1) && index >= 0) {
+        slides[index].classList.add('slider__item_active')
+    }
+
 }
 
 
 nextSlideButton.onclick = function () {
-    slider(index += 1)
+    slider(indexMain = indexMain + 1)
 }
 
 previousSlideButton.onclick = function () {
-    slider(index -= 1)
+    slider(indexMain = indexMain - 1)
 }
 
 
